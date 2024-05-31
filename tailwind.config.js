62b087e21f977d0a48fec19952e94cc10098452c
Victor Eleanya <victoreleanya89@gmail.com>
2024-05-31 16:57:34 +0100

    feat(banner): added banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      backgroundImage: {
+        "custom-green-gradient":
+          "linear-gradient(to bottom, #40A433 0%, #40A433 100%)",
+      },
       colors: {
         primary: "#2A8420",
         secondary: "#F9DEDC",
